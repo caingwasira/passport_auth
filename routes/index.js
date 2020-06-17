c67871c51b7e8931de63d2451095b269f49eac0f
Cain <ctgwasira@gmail.com>
2020-06-17 13:38:00 +0200

    Updated with a video

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 // Welcome page
-router.get('/', (req, res) => res.render('welcome'));
+router.get('/', (req, res) => {
+    res.render('welcome');
+});
 
 
 // Dashboard page
